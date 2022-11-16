e7930f78ecddceb025b370250e107025bf1d56b2
Sergio Paucara <sergiopaucara@gmail.com>
2022-11-15 23:11:07 -0400

    task 3 - CRUD

--- src/test/java/testSuite/todoist/CRUDProjectTest.java
+++ src/test/java/testSuite/todoist/CRUDProjectTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class CRUDProjectTest extends TestBase{
     @Test
-    public void test() throws InterruptedException {
+    public void test() {
         String nameProject = "nuevo project sergio";
         String updateNameProject = "update name project";
 
